6967b5e30d37708b472d1dd2075290407fede6c7
valentinm35 <43413392+valentinm35@users.noreply.github.com>
2018-10-03 10:08:11 +0200

    liste

--- Exercice liste/js/main.js
+++ Exercice liste/js/main.js
@@ -2,9 +2,9 @@ fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
 	return res.json().then(function(data) {
 		console.log(data);
     var stations = data.network.stations;
-		var liste = document.querySelector('div');
+		var liste = document.querySelector('ul');
     for (var i=0; i < stations.length; i++){
-					var currentLi = document.createElement('p');
+					var currentLi = document.createElement('li');
 					var textnode = document.createTextNode(stations[i].name + '//' + stations[i].empty_slots + '//' + stations[i].free_bikes);         // Create a text node
 					currentLi.appendChild(textnode);
 					liste.appendChild(currentLi);
